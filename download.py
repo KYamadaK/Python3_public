92489c90f6a6bfd85ca370f42217c952ff860f7e
yamada999 <masterhinagiku@gmail.com>
2019-12-23 20:51:09 +0900

    新規作成
    第１引数にURL
    第２引数に保存するファイル名
    URLをリクエストしてファイルに保存する。
    
    ひょっとするとgetメソッドのほうが良いのかもしれない。

---
+++ download.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+import urllib.request
+import sys
+
+def download():
+
+    url = sys.argv[1]
+    title = sys.argv[2]
+    urllib.request.urlretrieve(url,"{0}".format(title))
+
+if __name__ == "__main__":
+    download()
